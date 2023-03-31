e5ee78bfcbc996cf440f5a52f68d5ca92b0f8881
Ashley Jacob <128179385+Asjacob25@users.noreply.github.com>
2023-03-30 22:43:44 -0400

    created node, edge, and table classes

--- src/main/java/edu/wpi/cs3733/D23/teamQ/db/Node.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/db/Node.java
@@ -1,4 +1,59 @@
 package edu.wpi.cs3733.D23.teamQ.db;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class Node {
+    private String nodeID;
+    private int xCoord;
+    private int yCoord;
+    private String floor;
+    private String building;
+    private List<Edge> edges;
+    private Location location;
+    private int locID;
+
+    Node(
+            String nodeID,
+            int xCoord,
+            int yCoord,
+            String floor,
+            String building,
+            List<Edge> edges,
+            Location location) {
+        this.nodeID = nodeID;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.floor = floor;
+        this.building = building;
+        this.edges = edges;
+        this.location = location;
+
+
+        String x = Integer.toString(xCoord);
+        String y = Integer.toString(yCoord);
+        String xy = x + y;
+        this.locID = Integer.parseInt(xy);
+    }
+
+    public String nodeToString() {
+        return "nodeID: "
+                + this.nodeID
+                + ", xCoord: "
+                + this.xCoord
+                + ", yCoord: "
+                + this.yCoord
+                + ", floor: "
+                + this.floor
+                + ", building: "
+                + this.building
+                + ", edges: "
+                + this.edges
+                + ", location: "
+                + this.location;
+    }
 }

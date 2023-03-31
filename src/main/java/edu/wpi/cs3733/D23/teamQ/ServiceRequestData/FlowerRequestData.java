c5941c6ec95503725ed1c26882e761927810e999
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-03-31 16:07:52 -0400

    restarted setting up superclasses

---
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/ServiceRequestData/FlowerRequestData.java
@@ -0,0 +1,7 @@
+package edu.wpi.cs3733.D23.teamQ.ServiceRequestData;
+
+public class FlowerRequestData extends ServiceRequestData {
+    public FlowerRequestData(String assignee, String roomNumber, String specialInstructions) {
+        super(assignee, roomNumber, specialInstructions);
+    }
+}
---
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/ServiceRequestData/ServiceRequestData.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs3733.D23.teamQ.ServiceRequestData;
+
+public class ServiceRequestData {
+    private String assignee;
+    private String roomNumber;
+    private String specialInstructions;
+
+    public ServiceRequestData(String assignee, String roomNumber, String specialInstructions) {
+        this.assignee = assignee;
+        this.roomNumber = roomNumber;
+        this.specialInstructions = specialInstructions;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceRequestData{" +
+                "assignee='" + assignee + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                ", specialInstructions='" + specialInstructions + '\'' +
+                '}';
+    }
+}

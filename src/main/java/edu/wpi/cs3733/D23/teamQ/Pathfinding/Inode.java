3a5c04a54e857ea878b577c0cb081b1baacf0797
dovushman <52638904+dovushman@users.noreply.github.com>
2023-04-02 14:34:27 -0400

    fixed merge extras

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Inode.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Inode.java
@@ -1,15 +1,10 @@
 package edu.wpi.cs3733.D23.teamQ.Pathfinding;
 
-<<<<<<< HEAD
-public interface Inode {}
-=======
-import edu.wpi.cs3733.D23.teamQ.db.obj.Node;
-
 public interface Inode {
 
-    public int getNodeID(Node Anode);
+  public int getNodeID(Node Anode);
 
-    public int getX(Node Anode);
-    public int getY(Node Anode);
+  public int getX(Node Anode);
+
+  public int getY(Node Anode);
 }
->>>>>>> 0a91c9dd3bd7c029cf72126a77f6db666c5bca9b

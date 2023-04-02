0a91c9dd3bd7c029cf72126a77f6db666c5bca9b
odzabolotnev <odzabolotnev@wpi.edu>
2023-04-02 14:25:25 -0400

    Interface for nodes

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Inode.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Inode.java
@@ -1,4 +1,11 @@
 package edu.wpi.cs3733.D23.teamQ.Pathfinding;
 
+import edu.wpi.cs3733.D23.teamQ.db.obj.Node;
+
 public interface Inode {
+
+    public int getNodeID(Node Anode);
+
+    public int getX(Node Anode);
+    public int getY(Node Anode);
 }

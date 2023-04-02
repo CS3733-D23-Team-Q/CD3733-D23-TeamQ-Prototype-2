5d22e0c676b4b507ad377cb0c5079866bb433e6a
dovushman <52638904+dovushman@users.noreply.github.com>
2023-04-02 16:41:35 -0400

    code cleanup

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Node.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/Node.java
@@ -3,15 +3,12 @@ package edu.wpi.cs3733.D23.teamQ.Pathfinding;
 import java.util.List;
 
 public class Node implements Comparable<Node> {
-  // Id for readability of result purposes
   private static int idCounter = 0;
   private int id;
 
-  // Parent in the path
   Node parent = null;
 
   private List<Edge> neighbors;
-  //
   //  // Evaluation functions
   double f = Double.MAX_VALUE;
   double g = Double.MAX_VALUE;
@@ -19,44 +16,22 @@ public class Node implements Comparable<Node> {
   private int yCoord;
 
   Node(int xCoord, int yCoord) {
-    //        this.h = h;
     this.xCoord = xCoord;
     this.yCoord = yCoord;
-    //    this.id = idCounter++;
-    //    this.neighbors = new ArrayList<>();
   }
   //
   public List<Edge> getNeighbors() {
     return neighbors;
   }
-  //
-  //  public static int getIdCounter() {
-  //    return idCounter;
-  //  }
-  //
-  //  public int getId() {
-  //    return id;
-  //  }
-  //
-  //  public Node getParent() {
-  //    return parent;
-  //  }
-  //
+
   public double getF() {
     return f;
   }
-  //
+
   //  @Override
   public int compareTo(Node n) {
     return Double.compare(this.f, n.f);
   }
-  //
-  //  public void addBranch(int weight, Node node) {
-  //    Edge newEdge = new Edge(weight, node);
-  //    Edge inverseEdge = new Edge(weight, this);
-  //    neighbors.add(newEdge);
-  //    neighbors.add(inverseEdge);
-  //  }
 
   public int getxCoord() {
     return xCoord;
--- src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/PathMain.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Pathfinding/PathMain.java
@@ -65,23 +65,7 @@ public class PathMain {
     n12.addBranch(1, n13);
     n13.addBranch(1, n7);
     n13.addBranch(7, n12);
-    //
-    //        head.addBranch(1, n1);
-    //        head.addBranch(5, n2);
-    //        head.addBranch(2, n3);
-    //        n3.addBranch(1, n2);
-    //
-    //        Node n4 = new Node(1);
-    //        Node n5 = new Node(1);
-    //        Node target = new Node(1,5);
-    //
-    //        n1.addBranch(n4.getWeight(n4, target), n4);
-    //        n2.addBranch(4, n5);
-    //        n3.addBranch(6, n4);
-    //
-    //        n4.addBranch(3, target);
-    //        n5.addBranch(1, n4);
-    //        n5.addBranch(3, target);
+
 
     Node res = aStar(head, n8);
     printPath((nodeMethods) res);

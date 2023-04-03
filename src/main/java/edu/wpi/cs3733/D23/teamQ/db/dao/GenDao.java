ce6da2eea4c62cb2b73b5b316b7eb29d10e1a425
Kelu Liu <keluliu5@gmail.com>
2023-04-03 12:22:37 -0400

    started accountDAO reformat

--- src/main/java/edu/wpi/cs3733/D23/teamQ/db/dao/GenDao.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/db/dao/GenDao.java
@@ -1,8 +1,11 @@
 package edu.wpi.cs3733.D23.teamQ.db.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 public interface GenDao<T, G> {
+
   public List<T> getAllRows();
 
   public T retrieveRow(G ID);

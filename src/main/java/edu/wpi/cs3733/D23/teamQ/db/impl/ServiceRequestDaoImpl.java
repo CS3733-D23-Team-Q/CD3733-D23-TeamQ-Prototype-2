db5979d5b0aa6098fef75dcd8385b427c1c7bc83
cam40419 <83707970+cam40419@users.noreply.github.com>
2023-04-04 16:08:50 -0400

    Fixed SQLException in ServiceRequestDaoImpl

--- src/main/java/edu/wpi/cs3733/D23/teamQ/db/impl/ServiceRequestDaoImpl.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/db/impl/ServiceRequestDaoImpl.java
@@ -12,41 +12,37 @@ import javafx.collections.ObservableList;
 public class ServiceRequestDaoImpl implements GenDao<ServiceRequest, Integer> {
 
   public ObservableList<ServiceRequest> getAllRows() {
-    try {
-      ObservableList<ServiceRequest> srL = FXCollections.observableArrayList();
-      FlowerRequestDaoImpl requestF = new FlowerRequestDaoImpl();
-      ConferenceRequestDaoImpl requestC = new ConferenceRequestDaoImpl();
-      List<FlowerRequest> flowerRequests = requestF.getAllRows();
-      List<ConferenceRequest> conferenceRequests = requestC.getAllRows();
+    ObservableList<ServiceRequest> srL = FXCollections.observableArrayList();
+    FlowerRequestDaoImpl requestF = new FlowerRequestDaoImpl();
+    ConferenceRequestDaoImpl requestC = new ConferenceRequestDaoImpl();
+    List<FlowerRequest> flowerRequests = requestF.getAllRows();
+    List<ConferenceRequest> conferenceRequests = requestC.getAllRows();
 
-      for (int i = 0; i < flowerRequests.size(); i++) {
-        FlowerRequest fr = flowerRequests.get(i);
-        ServiceRequest s =
-            new ServiceRequest(
-                fr.getRequestID(),
-                fr.getRequester(),
-                fr.getProgress(),
-                fr.getAssignee(),
-                fr.getRoomNumber(),
-                fr.getSpecialInstructions());
-        srL.add(s);
-      }
-      for (int i = 0; i < conferenceRequests.size(); i++) {
-        ConferenceRequest cr = conferenceRequests.get(i);
-        ServiceRequest s =
-            new ServiceRequest(
-                cr.getRequestID(),
-                cr.getRequester(),
-                cr.getProgress(),
-                cr.getAssignee(),
-                cr.getRoomNumber(),
-                cr.getSpecialInstructions());
-        srL.add(s);
-      }
-      return srL;
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+    for (int i = 0; i < flowerRequests.size(); i++) {
+      FlowerRequest fr = flowerRequests.get(i);
+      ServiceRequest s =
+          new ServiceRequest(
+              fr.getRequestID(),
+              fr.getRequester(),
+              fr.getProgress(),
+              fr.getAssignee(),
+              fr.getRoomNumber(),
+              fr.getSpecialInstructions());
+      srL.add(s);
     }
+    for (int i = 0; i < conferenceRequests.size(); i++) {
+      ConferenceRequest cr = conferenceRequests.get(i);
+      ServiceRequest s =
+          new ServiceRequest(
+              cr.getRequestID(),
+              cr.getRequester(),
+              cr.getProgress(),
+              cr.getAssignee(),
+              cr.getRoomNumber(),
+              cr.getSpecialInstructions());
+      srL.add(s);
+    }
+    return srL;
   }
 
   @Override

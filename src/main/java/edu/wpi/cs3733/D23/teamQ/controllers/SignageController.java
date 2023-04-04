5b874eb1cc7e866b75697e5fce09d08e2eca5a58
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-04 18:45:19 -0400

    signage updated visually, still need navigation added

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
@@ -1,7 +1,3 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-
-public class SignageController {
-}
+public class SignageController {}

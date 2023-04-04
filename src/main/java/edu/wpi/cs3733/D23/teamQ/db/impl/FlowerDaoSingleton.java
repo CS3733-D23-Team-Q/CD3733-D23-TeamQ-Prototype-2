98f159b345b440f4c89d7cd4e13d4e0f862e0cc3
cam40419 <83707970+cam40419@users.noreply.github.com>
2023-04-04 16:22:36 -0400

    Fixed SQLException in Singleton classes

--- src/main/java/edu/wpi/cs3733/D23/teamQ/db/impl/FlowerDaoSingleton.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/db/impl/FlowerDaoSingleton.java
@@ -1,6 +1,5 @@
 package edu.wpi.cs3733.D23.teamQ.db.impl;
 
-import java.sql.SQLException;
 import lombok.Getter;
 
 public enum FlowerDaoSingleton {
@@ -8,10 +7,6 @@ public enum FlowerDaoSingleton {
   @Getter private final FlowerRequestDaoImpl DaoFR;
 
   {
-    try {
-      DaoFR = new FlowerRequestDaoImpl();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
+    DaoFR = new FlowerRequestDaoImpl();
   }
 }

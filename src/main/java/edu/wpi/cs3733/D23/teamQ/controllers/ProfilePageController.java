b3e2efcd55cea78df435a7b705fc27ba78ee8b22
SophiaJohnn <sophiasunnyjohn@gmail.com>
2023-04-04 00:04:43 -0400

    tried working on profile

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ProfilePageController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ProfilePageController.java
@@ -1,79 +1,40 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
-import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXTextField;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import io.github.palexdev.materialfx.utils.SwingFXUtils;
-import javafx.event.ActionEvent;
+import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
+import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
-import javax.imageio.ImageIO;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class ProfilePageController {
-  @FXML private MFXButton ProfilePage_ChangePic_Button;
+  @FXML private Button Edit_Profile;
 
-  @FXML private MFXButton ProfilePage_Done_Button;
+  @FXML private Label Email_Display;
 
-  @FXML private MFXTextField ProfilePage_Email_TextField;
+  @FXML private Label First_Name_Display;
 
-  @FXML private MFXTextField ProfilePage_FirstName_TextField;
+  @FXML private Label ID_Number_Display;
 
-  @FXML private MFXTextField ProfilePage_IDNumber_TextField;
+  @FXML private Label Last_Name_Display;
 
-  @FXML private MFXTextField ProfilePage_LastName_TextField;
+  @FXML private Label Phone_Number_Display;
 
-  @FXML private MFXTextField ProfilePage_PhoneNumber_TextField;
+  @FXML private Label Title_Display;
 
-  @FXML private MFXTextField ProfilePage_Title_TextField;
-
-  @FXML private MFXTextField ProfilePage_Username_TextField;
-  @FXML private ImageView ProfilePage_Profile_Image;
-
-  private FileChooser fileChooser;
-  private File filePath;
+  @FXML private Label Username_Display;
 
   @FXML
   private void initialize() {
-    //            this.FlowerRequest_CancelButton.setOnMouseClicked(   MAKE THIS SO THAT WHEN DONE
-    // IS CLICKED SHOWS WHAT WAS CHANGED IN THE PROFILE PAGE
-    //                    event -> {
-    //                        Navigation.navigate(Screen.HOME);
-    //                    });
+    // DO ONE FOR THE IMAGE AS WELL
+    this.Title_Display.setText(Title_Display.getText());
+    this.ID_Number_Display.setText(ID_Number_Display.getText());
+    this.First_Name_Display.setText(First_Name_Display.getText());
+    this.Last_Name_Display.setText(Last_Name_Display.getText());
+    this.Last_Name_Display.setText(Last_Name_Display.getText());
+    this.Username_Display.setText(Username_Display.getText());
+    this.Email_Display.setText(Email_Display.getText());
+    this.Phone_Number_Display.setText(Phone_Number_Display.getText());
+
+    this.Edit_Profile.setOnMouseClicked(event -> Navigation.navigate(Screen.PROFILE_EDIT_PAGE));
   }
-
-    public void changeProfileImage(ActionEvent event) {
-      Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-      fileChooser = new FileChooser();
-      fileChooser.setTitle("Open image");
-
-      String userDirectoryString = System.getProperty("user.home") + "\\Pictures";
-      File userDirectory = new File(userDirectoryString);
-
-      if(!userDirectory.canRead())
-        userDirectory = new File("c:/");
-
-      fileChooser.setInitialDirectory(userDirectory);
-      this.filePath = fileChooser.showOpenDialog(stage);
-
-      try {
-        BufferedImage bufferedImage = ImageIO.read(filePath);
-        Image image = SwingFXUtils.toFXImage(bufferedImage,null);
-        ProfilePage_Profile_Image.setImage(image);
-        photo.setImage(ProfilePage_Profile_Image.getImage());
-      } catch (IOException e)
-      {
-        System.err.println(e.getMessage());
-      }
-
-    }
 }

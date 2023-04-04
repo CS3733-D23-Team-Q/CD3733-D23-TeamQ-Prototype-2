76b09ac818362ed8024ec0e33f6fffe3a0cac452
wmerry25 <114078333+wmerry25@users.noreply.github.com>
2023-04-04 12:09:42 -0400

    retrieve row added

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ListServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/ListServiceRequestController.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
+import edu.wpi.cs3733.D23.teamQ.db.impl.ConferenceRequestDaoImpl;
+import edu.wpi.cs3733.D23.teamQ.db.impl.FlowerRequestDaoImpl;
 import edu.wpi.cs3733.D23.teamQ.db.impl.ServiceRequestDaoImpl;
 import edu.wpi.cs3733.D23.teamQ.db.obj.ServiceRequest;
 import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
@@ -11,6 +13,8 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
 
 public class ListServiceRequestController {
   @FXML Button homeButton;
@@ -28,6 +32,12 @@ public class ListServiceRequestController {
 
   ServiceRequestDaoImpl serviceRequestDao = new ServiceRequestDaoImpl();
 
+  FlowerRequestDaoImpl flowerRequestDao = new FlowerRequestDaoImpl();
+
+  ConferenceRequestDaoImpl conferenceRequestDao = new ConferenceRequestDaoImpl();
+
+  public ListServiceRequestController() throws SQLException {
+  }
   @FXML
   public void initialize() {
     requestID.setCellValueFactory(new PropertyValueFactory<ServiceRequest, Integer>("requestID"));
@@ -48,19 +58,12 @@ public class ListServiceRequestController {
 
   @FXML
   public void selectButtonClicked() {
-    System.out.println(
-        "ID: " + tableView.getSelectionModel().getSelectedItems().get(0).getRequestID());
-    System.out.println(
-        "Progress: " + tableView.getSelectionModel().getSelectedItems().get(0).getProgress());
-    System.out.println(
-        "Requester: " + tableView.getSelectionModel().getSelectedItems().get(0).getRequester());
-    System.out.println(
-        "Assignee: " + tableView.getSelectionModel().getSelectedItems().get(0).getAssignee());
-    System.out.println(
-        "Room Number: " + tableView.getSelectionModel().getSelectedItems().get(0).getRoomNumber());
-    System.out.println(
-        "Special Instructions: "
-            + tableView.getSelectionModel().getSelectedItems().get(0).getSpecialInstructions());
+    if(flowerRequestDao.retrieveRow(tableView.getSelectionModel().getSelectedItems().get(0).getRequestID()) != null){
+
+    }
+    else if(conferenceRequestDao.retrieveRow(tableView.getSelectionModel().getSelectedItems().get(0).getRequestID()) != null){
+
+    }
   }
 
   @FXML

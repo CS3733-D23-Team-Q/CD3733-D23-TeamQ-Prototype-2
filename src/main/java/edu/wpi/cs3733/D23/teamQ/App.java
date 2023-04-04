f70cf92d93a3298c5c777196db81060d34252e98
yuranxue272181 <xue272181@gmail.com>
2023-04-04 15:17:42 -0400

    update

--- src/main/java/edu/wpi/cs3733/D23/teamQ/App.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/App.java
@@ -42,7 +42,7 @@ public class App extends Application {
                 .getResource("/edu/wpi/cs3733/D23/teamQ/views/styles/Home.css")
                 .toExternalForm());
     primaryStage.show();
-    Navigation.navigate(Screen.);
+    Navigation.navigate(Screen.HOME);
     primaryStage.centerOnScreen();
   }
 

ac953a7f643a49e65f676085c97937f2ec073fe5
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-04 18:16:23 -0400

    signage fix in progress

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
@@ -1,17 +1,7 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
-import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
-import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 
 public class SignageController {
-
-  @FXML private Button BackHomeBTN;
-
-  /** initialize the BackHomeBTN, Navigate to the home page when the button is clicked. */
-  @FXML
-  public void initialize() {
-    BackHomeBTN.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
-  }
 }

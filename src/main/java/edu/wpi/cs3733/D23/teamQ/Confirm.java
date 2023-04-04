f33a39c657eeaaa0f1c24c243b73ed7f1b93bd9b
Kelu Liu <keluliu5@gmail.com>
2023-04-03 20:39:55 -0400

    restructured accountDAO and account functionalities

--- src/main/java/edu/wpi/cs3733/D23/teamQ/Confirm.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/Confirm.java
@@ -10,14 +10,17 @@ import javafx.stage.Stage;
 public class Confirm {
   public Scene getScene(Stage stage, String title, String message) {
     stage.setTitle(title);
+
     Label label = new Label();
     label.setText(message);
     label.setStyle("-fx-font-weight: bold");
     Button okButton = new Button("Ok");
     okButton.setOnAction(e -> stage.close());
+
     VBox layout = new VBox(5);
     layout.getChildren().addAll(label, okButton);
     layout.setAlignment(Pos.CENTER);
+
     Scene scene = new Scene(layout, 400, 300);
     return scene;
   }

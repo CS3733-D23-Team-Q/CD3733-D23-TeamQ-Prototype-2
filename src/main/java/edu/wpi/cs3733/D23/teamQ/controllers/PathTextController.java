49485d34df593c4f46ea6be17f1f717d96be94be
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2023-04-04 21:34:42 -0400

    added sout for textual path

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathTextController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/PathTextController.java
@@ -1,8 +1,14 @@
 package edu.wpi.cs3733.D23.teamQ.controllers;
 
+import static edu.wpi.cs3733.D23.teamQ.Pathfinding.star.aStar;
+
+import edu.wpi.cs3733.D23.teamQ.Pathfinding.star;
+import edu.wpi.cs3733.D23.teamQ.db.Qdb;
+import edu.wpi.cs3733.D23.teamQ.db.obj.Node;
 import edu.wpi.cs3733.D23.teamQ.navigation.Navigation;
 import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.sql.SQLException;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -34,7 +40,12 @@ public class PathTextController {
   }
 
   @FXML
-  public void submitButtonClicked() {
+  public void submitButtonClicked() throws SQLException {
+    Qdb qdb = Qdb.getInstance();
+    Node start = qdb.nodeTable.retrieveRow(Integer.parseInt(startNodeField.getText()));
+    Node end = qdb.nodeTable.retrieveRow(Integer.parseInt(endNodeField.getText()));
+    String textPath = star.returnPath(aStar(start, end));
+    System.out.println(textPath);
     Navigation.navigate(Screen.HOME);
   }
 

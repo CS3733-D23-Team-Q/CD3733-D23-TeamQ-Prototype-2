9dfe16db68bb563de748fe3746e81e8cccf5c9c3
Kenyon Coleman <kenyon.coleman15@gmail.com>
2023-04-05 10:42:32 -0400

    minor fixes to node and edge

--- src/main/java/edu/wpi/cs3733/D23/teamQ/db/obj/ConferenceRequest.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/db/obj/ConferenceRequest.java
@@ -6,33 +6,33 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ConferenceRequest extends ServiceRequest {
-    private String dateTime;
-    private String foodChoice;
+  private String dateTime;
+  private String foodChoice;
 
-    public ConferenceRequest(
-            int requestID,
-            String requester,
-            int progress,
-            String assignee,
-            String roomNumber,
-            String specialInstructions,
-            String dateTime,
-            String foodChoice) {
-        super(requestID, requester, progress, assignee, roomNumber, specialInstructions);
-        this.dateTime = dateTime;
-        this.foodChoice = foodChoice;
-    }
+  public ConferenceRequest(
+      int requestID,
+      String requester,
+      int progress,
+      String assignee,
+      String roomNumber,
+      String specialInstructions,
+      String dateTime,
+      String foodChoice) {
+    super(requestID, requester, progress, assignee, roomNumber, specialInstructions);
+    this.dateTime = dateTime;
+    this.foodChoice = foodChoice;
+  }
 
-    public ConferenceRequest(
-            String requester,
-            int progress,
-            String assignee,
-            String roomNumber,
-            String specialInstructions,
-            String dateTime,
-            String foodChoice) {
-        super(0, requester, progress, assignee, roomNumber, specialInstructions);
-        this.dateTime = dateTime;
-        this.foodChoice = foodChoice;
-    }
+  public ConferenceRequest(
+      String requester,
+      int progress,
+      String assignee,
+      String roomNumber,
+      String specialInstructions,
+      String dateTime,
+      String foodChoice) {
+    super(0, requester, progress, assignee, roomNumber, specialInstructions);
+    this.dateTime = dateTime;
+    this.foodChoice = foodChoice;
+  }
 }

c8c97c3fcd6ccd98acfb67ae039a7cf4f2cd1cb0
yuranxue272181 <xue272181@gmail.com>
2023-04-05 20:59:59 -0400

    add back home button and menu

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
@@ -5,11 +5,19 @@ import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 
 public class SignageController {
-  @FXML GridPane signageRoot;
+  @FXML private Button BackHomeBTN;
+
+  @FXML private MenuItem exitItem;
+
+  @FXML private MenuItem homeItem;
+
+  @FXML private GridPane signageRoot;
 
   @FXML
   public void initialize() {
@@ -29,5 +37,15 @@ public class SignageController {
                     }
                   });
         });
+
+    BackHomeBTN.setOnMouseClicked((event -> Navigation.navigate(Screen.HOME)));
+  }
+
+  public void homeItemClicked(javafx.event.ActionEvent actionEvent) {
+    Navigation.navigate(Screen.HOME);
+  }
+
+  public void exitItemClicked(javafx.event.ActionEvent actionEvent) {
+    Platform.exit();
   }
 }

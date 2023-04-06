5566e4a28cb0703c6e89cc1943c20b6a289f8151
yuranxue272181 <xue272181@gmail.com>
2023-04-05 21:27:59 -0400

    delete the change before

--- src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
+++ src/main/java/edu/wpi/cs3733/D23/teamQ/controllers/SignageController.java
@@ -5,19 +5,11 @@ import edu.wpi.cs3733.D23.teamQ.navigation.Screen;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 
 public class SignageController {
-  @FXML private Button BackHomeBTN;
-
-  @FXML private MenuItem exitItem;
-
-  @FXML private MenuItem homeItem;
-
-  @FXML private GridPane signageRoot;
+  @FXML GridPane signageRoot;
 
   @FXML
   public void initialize() {
@@ -37,15 +29,5 @@ public class SignageController {
                     }
                   });
         });
-
-    BackHomeBTN.setOnMouseClicked((event -> Navigation.navigate(Screen.HOME)));
-  }
-
-  public void homeItemClicked(javafx.event.ActionEvent actionEvent) {
-    Navigation.navigate(Screen.HOME);
-  }
-
-  public void exitItemClicked(javafx.event.ActionEvent actionEvent) {
-    Platform.exit();
   }
 }
